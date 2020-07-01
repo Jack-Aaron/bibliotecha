18bd227ee19d8cd3e6fc40d88976fc39f6afd75e
Jonathan Alpart <panjialang@gmail.com>
2020-07-01 16:01:08 -0500

    axios functions added for google books api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
+const BASEURL = 'https://www.googleapis.com/books/v1/volumes?q=';
+const IDBASEURL = 'https://www.googleapis.com/books/v1/volumes/';
+
 export default {
   getBooks: function () {
     return axios.get('/api/books');
   },
   saveBook: function (BookData) {
     return axios.post('/api/books', BookData);
+  },
+  searchBooks: function (query) {
+    return axios.get(BASEURL + query)
+  },
+  searchBooksById: function (id) {
+    return axios.get(IDBASEURL + id);
   }
 };
