5dee0bdded2452274ea872ef2f48ad62af74e49e
Jonathan Alpart <panjialang@gmail.com>
2020-07-01 14:52:18 -0500

    import bootstrap.min.css

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 // import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
