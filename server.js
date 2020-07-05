4e1292335082ab367eee48e40c1e99fdacb565af
Jonathan Alpart <panjialang@gmail.com>
2020-07-05 12:46:47 -0500

    fixing server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(routes);
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,6 +16,8 @@ if (process.env.NODE_ENV === 'production') {
     });
     console.log('Serving React App...');
   };
+
+app.use(routes);
 
 // MongoDB via Mongoose
 mongoose.connect(
